ae926a4b51105f62dccb19091b813f57828a939b
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-07-23 13:52:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,32 +42,34 @@ logging.info("\n\nStarting to search for Cowin vaccine slots!\n(c) @xditya")
 
 async def processes():  # sourcery no-metrics
     async with bot:
-        for i in auth:
-            await bot.send_message(i, "Bot Started.")
-        while True:
-            today = datetime.today().strftime("%d-%m-%Y")
-            base_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}".format(
-                pincode, today
-            )
-            res = get(base_url)
-            if res.status_code == 200:
-                json = res.json()
-                if json["secrets"]:
-                    msg = ""
-                    for i in json["secrets"]:
-                        if (
-                            i["min_age_limit"] <= age
-                            and i["available_capacity"] > 0
-                        ):
-                            msg += f"**Centre**: {i['name']}\n"
-                            msg += f"**Block**: {i['block_name']}\n"
-                            msg += f"**Price**: {i['fee_type']}\n"
-                            msg += f"**Availablity**: {j['available_capacity']}\n"
-                            if i["vaccine"] != "":
-                                msg += f"**Vaccine type**: {j['vaccine']}"
-                            if msg != "":
-                                for i in auth:
-                                    await bot.send_message(i, msg)
+        msg = ""
+        for user in auth:
+            await bot.send_message(user, "Bot Started.")
+        today = datetime.today().strftime("%d-%m-%Y")
+        base_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}".format(
+             pincode, today
+        )
+        res = get(base_url)
+        if res.status_code == 200:
+            json = res.json()
+            if json["sessions"]:
+                for i in json["sessions"]:
+                    if (
+                        i["min_age_limit"] <= age
+                        and i["available_capacity"] > 0
+                    ):
+                        msg += f"**Centre**: {i['name']}\n"
+                        msg += f"**Block**: {i['block_name']}\n"
+                        msg += f"**Price**: {i['fee_type']}\n"
+                        msg += f"**Availablity**: {i['available_capacity']}\n"
+                        if i["vaccine"] != "":
+                            msg += f"**Vaccine type**: {i['vaccine']}"
+        if msg != "":
+            to_send = msg
+        else:
+            to_send = "**No vaccine Available**"
+        for user in auth:
+            await bot.send_message(user, to_send)
 
 
 bot.loop.run_until_complete(processes())
